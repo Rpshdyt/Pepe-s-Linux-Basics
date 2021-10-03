0740d9384d2f62cdee513a3e9f34405aa92809bb
Rpshdyt <37339095+Rpshdyt@users.noreply.github.com>
2021-10-03 17:07:32 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,4 +1,10 @@
 echo "this will install the following applications: git, VLC, OpenShot, python, pip, wine, htop, vtop, cmatrix, steam, virtualbox, gimp, audacity"
+echo "Do you wish to install this program?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) make install; break;;
+        No ) exit;;
+    esac
 sudo add-apt-repository multiverse
 sudo apt install git
 sudo apt install python
@@ -26,3 +32,4 @@ sudo apt install vlc
 sudo add-apt-repository ppa:openshot.developers/ppa
 sudo apt update
 sudo apt install openshot-qt
+done
