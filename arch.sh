e5ecc4411b5ec8e3bcd3601784f94b97e7fef2be
PepeSlayer <37339095+Rpshdyt@users.noreply.github.com>
2021-10-03 16:13:37 +0200

    added the command files for debian and arch
    added the commands for the debian and arch distros of linux. if anyone else wants to do some commands for other distros, feel free to do so.
    
    -Pepe

---
+++ arch.sh
@@ -0,0 +1,19 @@
+echo "this will -S the following applications: git, VLC, python, pip, wine, htop, vtop, cmatrix, steam, virtualbox, gimp, audacity"
+sudo add-pacman-repository multiverse
+sudo pacman -S git
+sudo pacman -S python
+sudo pacman -S python-pip
+sudo pacman -S wine
+sudo pacman -S htop
+sudo pacman -S cmatrix
+sudo pacman -S steam
+sudo pacman -S discord
+sudo pacman -S virtualbox
+sudo pacman -S gimp
+sudo pacman -S audacity
+sudo pacman -S ffmpeg
+sudo pacman -S obs-studio-minimal
+sudo pacman -S nodejs
+sudo pacman -S npm
+sudo pacman -S vtop
+sudo pacman -S vlc
---
+++ debian.sh
@@ -0,0 +1,28 @@
+echo "this will install the following applications: git, VLC, OpenShot, python, pip, wine, htop, vtop, cmatrix, steam, virtualbox, gimp, audacity"
+sudo add-apt-repository multiverse
+sudo apt install git
+sudo apt install python
+sudo apt install python-pip
+sudo apt install wine
+sudo apt install htop
+sudo apt install cmatrix
+sudo apt update
+sudo apt install steam
+sudo wget https://discord.com/api/download?platform=linux&format=tar.gz
+sudo tar -xvzf discord-0.0.10.tar.gz -C /opt
+sudo ln -sf /opt/Discord/Discord /usr/bin/Discord
+sudo cp -r /opt/Discord/discord.desktop /usr/share/applications
+sudo apt install virtualbox
+sudo apt install gimp
+sudo apt install audacity
+sudo apt install ffmpeg
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install obs-studio
+sudo apt install nodejs
+sudo apt install npm
+sudo npm install -g vtop
+sudo apt install vlc
+sudo add-apt-repository ppa:openshot.developers/ppa
+sudo apt update
+sudo apt install openshot-qt
